ef560e0a57eae6277948c8d1c684c036cbd91ac9
marvellouschirunga <marvellouschirunga@gmail.com>
2023-09-16 12:24:57 +0200

    adding contacts routes and .env file

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 
 app.use('/', require('./routes'));
 
-app.listen(process.env.PORT || 3000);
-console.log('Running on port ' + (process.env.PORT || 3000));
+
+mongodb.initDb((err) => {
+   if (err) {
+    console.log(err);
+   } 
+   else {
+    app.listen(process.env.PORT || 3000);
+    console.log('Database is listening and node running on port ' + (process.env.PORT || 3000));
+   }
+
+})
+
